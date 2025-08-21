314af04c384867f1587f094c28de719d1a9103ba
tss-aws <tss.formacaoaws@gmail.com>
2025-08-21 21:33:04 +0000

    disparando primeiro pipeline com push

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Adicionar Tarefas
+        Add Task
       </button>
       
       <Modal
